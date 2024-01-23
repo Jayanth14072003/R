61a6656b91cb49e7673e107be0a646de7ed71fbd
Benaka <102427589+Benaka1604@users.noreply.github.com>
2024-01-23 21:01:16 +0530

    Update lab06.r
    Minor error correction

--- lab06.r
+++ lab06.r
@@ -1,9 +1,8 @@
-#install.packages("Markovchain")
-#Library(markovchain)
-
+#install.packages("markovchain")
+library(markovchain)
 trans_matrix=matrix(c(0.8,0.2,0.4,0.6),nrow=2,ncol=2,byrow=TRUE)
 sts=c("State A","State B")
 my_markov_chain=new("markovchain",states=sts,transitionMatrix=trans_matrix)
-stnry_dist=steadystates(my_markov_chain)
+stnry_dist=steadyStates(my_markov_chain)
 cat("Stationary Distribution : \n")
 print(stnry_dist)
