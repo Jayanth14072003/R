5d7352c8580f5829a23628252003663e9fc17336
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2024-01-27 10:57:57 +0530

    Update lab06.r

--- lab06.r
+++ lab06.r
@@ -1,5 +1,6 @@
 #install.packages("markovchain")
-library(markovchain)
+#library(markovchain)
+
 trans_matrix=matrix(c(0.8,0.2,0.4,0.6),nrow=2,ncol=2,byrow=TRUE)
 sts=c("State A","State B")
 my_markov_chain=new("markovchain",states=sts,transitionMatrix=trans_matrix)
